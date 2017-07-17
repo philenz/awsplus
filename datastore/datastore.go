8fcbfc68b64d23945f3d1a9eda8c06eb2b4ebad8
Phil Evans <phil.evans@gallagher.com>
2017-07-17 15:32:48 +1200

    create tables in init()

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -2,6 +2,7 @@ package datastore
 
 import (
 	"database/sql"
+	_ "github.com/lib/pq"
 	"fmt"
 	"log"
 	"awsplus/config"
@@ -21,6 +22,31 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// create / populate tables if not already done...
+	createSecurityGroupSQL := fmt.Sprintf(
+		"CREATE TABLE security_group (" +
+			"id varchar(20) CONSTRAINT sg_pkey PRIMARY KEY," +
+			"vpc_id varchar(20) NOT NULL," +
+			"description varchar(300)" +
+			");")
+	Db.Exec(createSecurityGroupSQL)
+
+	createIngressSQL := fmt.Sprintf(
+		"CREATE TABLE ingress (" +
+			"sg_id varchar(20) REFERENCES security_group(id)," +
+			"protocol varchar(10) NOT NULL," +
+			"from_port integer NOT NULL," +
+			"to_port integer NOT NULL," +
+			"source varchar(40) NOT NULL," +
+			"UNIQUE (sg_id, protocol, from_port, source)" +
+			");")
+	Db.Exec(createIngressSQL)
+
 	return
+}
+
+func PrintMessage() {
+	fmt.Println("Hello from datastore :-)")
 }
 
